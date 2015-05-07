255097fbd365967bd80eba472101646c1b073a1c
jptucker <john.pelton.tucker@gmail.com>
2015-05-07 11:34:06 -0500

    Completed HW 6

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+    @photo = Photo.find_by({:id => params["id"]})
+
+  end
+
+  def new_form
+
+  end
+
+  def create_photo
+    @photo=Photo.new
+    @photo.caption=params["the_caption"]
+    @photo.source=params["the_source"]
+    @photo.save
+
+    redirect_to("/photos")
+  end
+
+  def destroy
+    photo = Photo.find_by({:id => params["id"]})
+    photo.destroy
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({:id => params["id"]})
+  end
+
+  def update_row
+    photo = Photo.find_by({:id => params["id"]})
+    photo.caption=params["the_caption"]
+    photo.source=params["the_source"]
+    photo.save
+    redirect_to("/photos/#{photo.id}")
+  end
 
 end
